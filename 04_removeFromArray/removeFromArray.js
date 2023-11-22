9f54dbc356834c034d8f316dcd871fdc2e3766ad
Surya Saputra <17169567+suryasaputra2016@users.noreply.github.com>
2023-11-22 16:11:50 +0700

    Fix removing other arguments from initial array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(initialArray, ...tobeRemoved) {
+    let finalArray = [];
+    for (item of initialArray) {
+        if (tobeRemoved.includes(item)) {
+            continue;
+        }
+        finalArray.push(item);
+    }
 
+    return finalArray;
 };
 
 // Do not edit below this line
