10991597a1880c6b8c1273616df1b03a20d022cf
Surya Saputra <17169567+suryasaputra2016@users.noreply.github.com>
2023-11-22 16:45:29 +0700

    Add temperature conversion code

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  return roundOneDecimal(5*(fahrenheit-32)/9); 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return roundOneDecimal(9*celsius/5 + 32); 
 };
+
+function roundOneDecimal(number) {
+  return Math.round(number*10)/10;
+}
 
 // Do not edit below this line
 module.exports = {
