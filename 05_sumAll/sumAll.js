1a3843689ecb32a8234beca1c136611dcd81e151
Surya Saputra <17169567+suryasaputra2016@users.noreply.github.com>
2023-11-22 16:22:58 +0700

    Fix the addition of integers between two numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,40 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    if(typeof(firstNumber) !== 'number' || typeof(secondNumber) !== 'number') {
+        return 'ERROR';
+    }
 
+    let smallNumber = min(firstNumber, secondNumber);
+    if(smallNumber<0) {
+        return 'ERROR';
+    }
+
+    let bigNumber = max(firstNumber, secondNumber);
+
+    let sum = 0;
+
+    for(let i = smallNumber; i<= bigNumber; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
+
+
+function min(firstNumber, secondNumber) {
+    if (firstNumber < secondNumber) {
+        return firstNumber;
+    } else {
+        return secondNumber;
+    }
+}
+
+function max(firstNumber, secondNumber) {
+    if (firstNumber > secondNumber) {
+        return firstNumber;
+    } else {
+        return secondNumber;
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
