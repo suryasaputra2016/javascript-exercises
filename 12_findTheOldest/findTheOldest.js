343a9d592ea30f576acaa6b9c53a01e7b0da9036
Surya Saputra <17169567+suryasaputra2016@users.noreply.github.com>
2023-12-01 14:19:47 +0700

    Finish findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    function age(person) {
+        if(!person.yearOfDeath) {
+            return new Date().getFullYear() - person.yearOfBirth;
+        }
 
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+
+    people.sort((firstPerson, secondPerson) => (age(firstPerson) < age(secondPerson) ? 1 : -1) );
+
+    return people[0];
 };
 
 // Do not edit below this line
