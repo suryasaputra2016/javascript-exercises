19d313420228925288a1ebada9619612dfff0a74
Surya Saputra <17169567+suryasaputra2016@users.noreply.github.com>
2023-12-01 13:16:53 +0700

    Finish palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    let lowerText = text.toLowerCase();
+    lowerText = lowerText.replace(/[ ,.!?:;]/g,'');
 
+    const length = lowerText.length;
+    const halfLength = length/2;
+
+    for(i=0;i<halfLength;i++) {
+        if(lowerText[i] !== lowerText[length-1-i]) {
+            return false
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
