73253ea9b831166b7ba2160e4f40253ad6949c1b
Surya Saputra <17169567+suryasaputra2016@users.noreply.github.com>
2023-12-01 13:49:54 +0700

    Finish fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let first = 1
+    let second = 1
+    if(number <= 0) {
+        return 'OOPS';
+    } else if(number<3) {
+        return second;
+    }
+    
+    for(let i=3 ; i<=number ; i++) {
+        [first, second] = [second, first+second];
+    }
 
+    return second;
 };
 
 // Do not edit below this line
